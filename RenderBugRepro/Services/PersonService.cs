fa47c15ed1373b3cc20959dfee9f3d510c9f0087
Alexandre Giard <giard.alexandre@gmail.com>
2025-07-02 22:47:12 -0500

    feat: periodic update

--- RenderBugRepro/Services/PersonService.cs
+++ RenderBugRepro/Services/PersonService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Bogus;
@@ -15,6 +17,8 @@ public class PersonService : IDisposable
     private readonly SourceCache<Person, int> _personCache;
     private readonly CompositeDisposable _disposables;
     private readonly ReadOnlyObservableCollection<Person> _people;
+    private readonly Faker<Person> _faker;
+    private readonly Random _random;
 
     public ReadOnlyObservableCollection<Person> People => _people;
 
@@ -22,6 +26,21 @@ public class PersonService : IDisposable
     {
         _personCache = new SourceCache<Person, int>(person => person.Id);
         _disposables = new CompositeDisposable();
+        _random = new Random();
+
+        // Setup the faker for updates
+        _faker = new Faker<Person>()
+            .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+            .RuleFor(p => p.LastName, f => f.Name.LastName())
+            .RuleFor(p => p.Email, f => f.Internet.Email())
+            .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber())
+            .RuleFor(p => p.DateOfBirth, f => f.Date.Between(DateTime.Now.AddYears(-65), DateTime.Now.AddYears(-18)))
+            .RuleFor(p => p.Address, f => f.Address.FullAddress())
+            .RuleFor(p => p.City, f => f.Address.City())
+            .RuleFor(p => p.Country, f => f.Address.Country())
+            .RuleFor(p => p.Company, f => f.Company.CompanyName())
+            .RuleFor(p => p.JobTitle, f => f.Name.JobTitle())
+            .RuleFor(p => p.Salary, f => f.Random.Decimal(30000, 150000));
 
         // Connect the cache to a ReadOnlyObservableCollection
         _personCache
@@ -33,11 +52,14 @@ public class PersonService : IDisposable
 
         // Generate initial data
         GenerateInitialData();
+
+        // Start periodic updates
+        StartPeriodicUpdates();
     }
 
     private void GenerateInitialData()
     {
-        var faker = new Faker<Person>()
+        var initialFaker = new Faker<Person>()
             .RuleFor(p => p.Id, f => f.IndexFaker + 1)
             .RuleFor(p => p.FirstName, f => f.Name.FirstName())
             .RuleFor(p => p.LastName, f => f.Name.LastName())
@@ -51,8 +73,58 @@ public class PersonService : IDisposable
             .RuleFor(p => p.JobTitle, f => f.Name.JobTitle())
             .RuleFor(p => p.Salary, f => f.Random.Decimal(30000, 150000));
 
-        var people = faker.Generate(1000);
+        var people = initialFaker.Generate(1000);
         _personCache.AddOrUpdate(people);
+    }
+
+    private void StartPeriodicUpdates()
+    {
+        // Create a timer that triggers every 200ms
+        Observable.Timer(TimeSpan.FromMilliseconds(200), TimeSpan.FromMilliseconds(200))
+            .ObserveOn(RxApp.TaskpoolScheduler) // Do the work off the UI thread
+            .Subscribe(_ => UpdateRandomEntries())
+            .DisposeWith(_disposables);
+    }
+
+    private void UpdateRandomEntries()
+    {
+        try
+        {
+            // Get current items from cache
+            var currentItems = _personCache.Items.ToList();
+            if (!currentItems.Any()) return;
+
+            // Randomly select 20-30 entries to update
+            var updateCount = _random.Next(20, 31); // 20 to 30 inclusive
+            var indicesToUpdate = Enumerable.Range(0, currentItems.Count)
+                .OrderBy(_ => _random.Next())
+                .Take(updateCount)
+                .ToList();
+
+            var updatedPeople = new List<Person>();
+
+            foreach (var index in indicesToUpdate)
+            {
+                var existingPerson = currentItems[index];
+                
+                // Create an updated person with the same ID but new data
+                var updatedPerson = _faker.Generate();
+                updatedPerson.Id = existingPerson.Id; // Keep the same ID
+
+                updatedPeople.Add(updatedPerson);
+            }
+
+            // Update all selected entries at once
+            if (updatedPeople.Any())
+            {
+                _personCache.AddOrUpdate(updatedPeople);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Log or handle the exception as needed
+            Console.WriteLine($"Error updating entries: {ex.Message}");
+        }
     }
 
     public void AddPerson(Person person)
